85ab449c65edbb0747716847facd75cd462278fd
bok9504 <bkjung@ways1.com>
2023-07-05 11:55:17 +0900

    type scale changed

--- MOT/deep_sort_pytorch/deep_sort/deep_sort.py
+++ MOT/deep_sort_pytorch/deep_sort/deep_sort.py
@@ -48,7 +48,7 @@ class DeepSort(object):
             x1, y1, x2, y2 = self._tlwh_to_xyxy(box)
             track_id = track.track_id
             cls_id = track.cls_id
-            outputs.append(np.array([x1, y1, x2, y2, cls_id, track_id], dtype=np.int))
+            outputs.append(np.array([x1, y1, x2, y2, cls_id, track_id], dtype=np.int64))
         if len(outputs) > 0:
             outputs = np.stack(outputs, axis=0)
         return outputs
